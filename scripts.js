86213b01c21dcc4efc1f6f69734c6188e9131119
viktekbl <77540645+Vikek@users.noreply.github.com>
2023-06-01 18:19:40 +0200

    added operate function and variables for later

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
+let a = 0;
+let b = 0;
+let operator = "";
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
