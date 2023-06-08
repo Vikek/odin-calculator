7ce5544c2b8e0299b3dffeef427d8ec35ee06396
viktekbl <77540645+Vikek@users.noreply.github.com>
2023-06-08 19:52:48 +0200

    added functionality to the buttons

--- scripts.js
+++ scripts.js
@@ -2,6 +2,75 @@ let a = 0;
 let b = 0;
 let operator = "";
 
+// event listeners for the number buttons
+document.querySelector(".zero").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "0";
+});
+
+document.querySelector(".one").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "1";
+});
+
+document.querySelector(".two").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "2";
+});
+
+document.querySelector(".three").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "3";
+});
+
+document.querySelector(".four").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "4";
+});
+
+document.querySelector(".five").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "5";
+});
+
+document.querySelector(".six").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "6";
+});
+
+document.querySelector(".seven").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "7";
+});
+
+document.querySelector(".eight").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "8";
+});
+
+document.querySelector(".nine").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "9";
+});
+//////////////////////////////////
+
+
+// event listeners for the operation buttons
+document.querySelector(".divide").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "/";
+});
+
+document.querySelector(".multiply").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "*";
+});
+
+document.querySelector(".subtract").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "-";
+});
+
+document.querySelector(".add").addEventListener("click", function() {
+    document.querySelector("#input").textContent += "+";
+});
+//////////////////////////////////
+
+document.querySelector(".dot").addEventListener("click", function() {
+    document.querySelector("#input").textContent += ".";
+});
+
+document.querySelector(".clear").addEventListener("click", function() {
+    document.querySelector("#input").textContent = "";
+});
+
 function operate(a, b, operator) {
     switch (operator) {
         case "+":
