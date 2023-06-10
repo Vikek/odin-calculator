33fcc76bbad3989098f62882893d1003799bf58c
viktekbl <77540645+Vikek@users.noreply.github.com>
2023-06-10 20:09:00 +0200

    created logic for the calculations

--- scripts.js
+++ scripts.js
@@ -1,45 +1,56 @@
-let a = 0;
-let b = 0;
-let operator = "";
+let a = null;
+let b = null;
+let operator = null;
+let hasOperated = false;
 
 // event listeners for the number buttons
 document.querySelector(".zero").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "0";
 });
 
 document.querySelector(".one").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "1";
 });
 
 document.querySelector(".two").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "2";
 });
 
 document.querySelector(".three").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "3";
 });
 
 document.querySelector(".four").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "4";
 });
 
 document.querySelector(".five").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "5";
 });
 
 document.querySelector(".six").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "6";
 });
 
 document.querySelector(".seven").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "7";
 });
 
 document.querySelector(".eight").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "8";
 });
 
 document.querySelector(".nine").addEventListener("click", function() {
+    checkOperation();
     document.querySelector("#input").textContent += "9";
 });
 //////////////////////////////////
@@ -47,19 +58,23 @@ document.querySelector(".nine").addEventListener("click", function() {
 
 // event listeners for the operation buttons
 document.querySelector(".divide").addEventListener("click", function() {
-    document.querySelector("#input").textContent += "/";
+    storeValue(document.querySelector("#input").textContent);
+    operator = "/";
 });
 
 document.querySelector(".multiply").addEventListener("click", function() {
-    document.querySelector("#input").textContent += "*";
+    storeValue(document.querySelector("#input").textContent);
+    operator = "*";
 });
 
 document.querySelector(".subtract").addEventListener("click", function() {
-    document.querySelector("#input").textContent += "-";
+    storeValue(document.querySelector("#input").textContent);
+    operator = "-";
 });
 
 document.querySelector(".add").addEventListener("click", function() {
-    document.querySelector("#input").textContent += "+";
+    storeValue(document.querySelector("#input").textContent);
+    operator = "+";
 });
 //////////////////////////////////
 
@@ -68,24 +83,70 @@ document.querySelector(".dot").addEventListener("click", function() {
 });
 
 document.querySelector(".clear").addEventListener("click", function() {
-    document.querySelector("#input").textContent = "";
+    clearInput();
+    resetValues();
 });
 
+document.querySelector(".equals").addEventListener("click", function() {
+    if(a && operator) {
+        b = document.querySelector("#input").textContent;
+        a = +a;
+        b = +b;
+        operate(a, b, operator);
+    } else return;
+});
+
+function checkOperation() {
+    if(hasOperated) { //if operation is true clear input window before adding new numbers
+        clearInput()
+        hasOperated = false;
+    }
+}
+
+function clearInput() {
+    document.querySelector("#input").textContent = "";
+}
+
+function resetValues() {
+    a = null;
+    b = null;
+    operator = null;
+}
+
+function storeValue(value) {
+    if(a && operator) {
+        b = value;
+        a = +a;
+        b = +b;
+        operate(a, b, operator);
+    } else {
+        a = value;
+        hasOperated = true;
+    }
+    console.log(a);
+    console.log(b);
+}
+
 function operate(a, b, operator) {
+    let result = 0;
     switch (operator) {
         case "+":
-            add(a, b);
+            result = add(a, b);
             break;
         case "-":
-            subtract(a, b);
+            result = subtract(a, b);
             break;
         case "*":
-            multiply(a, b);
+            result = multiply(a, b);
             break;
         case "/":
-            divide(a, b);
+            result = divide(a, b);
             break;
     }
+    document.querySelector("#input").textContent = result;
+    resetValues();
+    storeValue(result);
+    hasOperated = true;
 }
 
 function add(a, b) {
